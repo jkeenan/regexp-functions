82d84bb633a43d55452fa3d8abd7b8230f0c1288
James E Keenan <jkeenan@cpan.org>
2017-08-12 11:05:42 -0400

    Initial commit; created by modulemaker.

---
+++ lib/Regexp/Functions/chmod_et_al.pm
@@ -0,0 +1,70 @@
+package Regexp::Functions::chmod_et_al;
+use strict;
+
+BEGIN {
+    use Exporter ();
+    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+    $VERSION     = '0.01';
+    @ISA         = qw(Exporter);
+    #Give a hoot don't pollute, do not export more than needed by default
+    @EXPORT      = qw();
+    @EXPORT_OK   = qw();
+    %EXPORT_TAGS = ();
+}
+
+
+=head1 NAME
+
+Regexp::Functions::chmod_et_al - Patterns for matching Perl functions chmod(), mkdir(), File::Path::mkpath(), File::Path::makepath()
+
+=head1 SYNOPSIS
+
+  use Regexp::Functions::chmod_et_al;
+  blah blah blah
+
+
+=head1 DESCRIPTION
+
+Stub documentation for this module was created by ExtUtils::ModuleMaker.
+It looks like the author of the extension was negligent enough
+to leave the stub unedited.
+
+Blah blah blah.
+
+
+=head1 USAGE
+
+
+
+=head1 BUGS
+
+
+
+=head1 SUPPORT
+
+
+
+=head1 AUTHOR
+
+    James E Keenan
+    CPAN ID: JKEENAN
+    jkeenan@cpan.org
+    http://thenceforward.net/perl/modules
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
+
+1;
+
