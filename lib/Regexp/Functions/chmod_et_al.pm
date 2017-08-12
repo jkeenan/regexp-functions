6c4f1e856758a4a9c02dcee888423cf76efd18a5
James E Keenan <jkeenan@cpan.org>
2017-08-12 11:28:34 -0400

    Add documentation in POD format.

--- lib/Regexp/Functions/chmod_et_al.pm
+++ lib/Regexp/Functions/chmod_et_al.pm
@@ -113,41 +113,57 @@ $qr_chmod_et_al = qr/
 
 =head1 NAME
 
-Regexp::Functions::chmod_et_al - Patterns for matching Perl functions chmod(), mkdir(), File::Path::mkpath(), File::Path::makepath()
+Regexp::Functions::chmod_et_al - Patterns for matching Perl functions chmod(), mkdir(), File::Path::mkpath(), File::Path::make_path()
 
 =head1 SYNOPSIS
 
-  use Regexp::Functions::chmod_et_al;
-  blah blah blah
+  use Regexp::Functions::chmod_et_al qw( $qr_chmod_et_al );
 
+  @captures = 'chmod 0000, $somedir' =~ $qr_chmod_et_al;
 
 =head1 DESCRIPTION
 
-Stub documentation for this module was created by ExtUtils::ModuleMaker.
-It looks like the author of the extension was negligent enough
-to leave the stub unedited.
+This module exports on request only a single scalar, C<$qr_chmod_et_al>, which
+is a compiled regular expression.  The regex has a B<very> limited focus:
+Searching Perl source code for instances of the following functions:
 
-Blah blah blah.
+=over 4
 
+=item * Perl built-in C<chmod>
 
-=head1 USAGE
+=item * Perl built-in C<mkdir>
 
+=item * C<File::Path::mkpath()>
 
+=item * C<File::Path::make_path()>
+
+=back
+
+... and only in circumstances where a numerical mode that specifies that the
+user has neither C<read> nor C<execute> is being supplied as an argument.
+Examples of such modes:
+
+    0000
+     000
+      00
+       0
+    0200
+     200
+    0222
+
+B<NOTE:>  This is alpha code.  This is a developer's tool and is B<not>
+intended be used in production code.
 
 =head1 BUGS
 
-
-
-=head1 SUPPORT
-
-
+None known at this time.
 
 =head1 AUTHOR
 
     James E Keenan
     CPAN ID: JKEENAN
     jkeenan@cpan.org
-    http://thenceforward.net/perl/modules
+    http://thenceforward.net/perl/modules/Regexp-Functions-chmod_et_al
 
 =head1 COPYRIGHT
 
@@ -156,7 +172,6 @@ it and/or modify it under the same terms as Perl itself.
 
 The full text of the license can be found in the
 LICENSE file included with this module.
-
 
 =head1 SEE ALSO
 
